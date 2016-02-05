3d8acdbbeddeb510c528e2994614b02219ea8c38
GAYLE BENDER <gayle.l.bender@jpmorgan.com>
2016-02-05 13:23:21 -0500

    more command line args and and methods

--- find_pr_containing_commit.rb
+++ find_pr_containing_commit.rb
@@ -1,6 +1,19 @@
+# ===================================
+# USAGE:
+# find_pr_containing_commit.rb # you will be prompted for repo and commit hash
+# find_pr_containing_commit.rb <repo> <commit_hash>
+# repo = rsam|self_service|rsam_core, etc
+#
+# ===================================
+
 require 'json'
 require 'net/http'
 require 'pry'
+
+# ===================================
+# CONSTANTS
+# ===================================
+NUM_RESULTS_AT_A_TIME = 10
 
 # ===================================
 # ENV VARIABLES
@@ -16,19 +29,29 @@ elsif PASSWORD.nil?
   exit(0)
 end
 
+# ===================================
+# ARGS
+# ===================================
+if ARGV.length == 2
+  @repo = ARGV[0]
+  @commit = ARGV[1]
+else
+  print "which repo do you want to look in? "
+  @repo = gets.chomp
+  print "which commit hash are you looking for? "
+  @commit = gets.chomp
+end
 
 # ===================================
 # HELPERS
 # ===================================
 
-def get_prs
+def get_prs(start)
   # TODO make some of these commmand line args
   base_url = 'https://stash-prod2.us.jpmchase.net:8443/rest/api/1.0/projects/RSAM/repos'
-  repo = 'rsam' # The repo name in stash
   type = 'pull-requests'
-  # id = '2024'
-  url = "#{base_url}/#{repo}/#{type}"
-  params = {'state' => 'MERGED', 'order' => 'NEWEST'}
+  url = "#{base_url}/#{@repo}/#{type}"
+  params = {'state' => 'MERGED', 'order' => 'NEWEST', 'limit' => NUM_RESULTS_AT_A_TIME, 'start' =>  start}
 
   uri = URI(url)
   uri.query = URI.encode_www_form(params)
@@ -40,20 +63,61 @@ def get_prs
   response = http.request(req)
 
   json_response = JSON.parse(response.body)
-  binding.pry
+  #binding.pry
   json_response
+end
+
+def pr_containing_commit(pull_requests, commit_hash)
+  binding.pry
+  puts "HI"
 end
 
 # ===================================
 # RUN
 # ===================================
 
-start = Time.now
-json_response = get_prs
+# This doesn't work.  For some reason the gets tries to use the first command line arg.
+# e.g. `gets': No such file or directory - rsam
+# begin
+#   json_response = get_prs
+#   # do stuff
+#   print "commit not found yet.  Continue? (y/n) " # if not_found
+#   binding.pry
+#   keep_going = gets.chomp
+# end until keep_going != "y"
+
+
+pull_requests1 = get_prs(0)
+pull_requests2 = get_prs(10)
+pull_requests3 = get_prs(20)
+pull_requests4 = get_prs(30)
+
+
+# Note in Ruby 2 you can use: "1.step(NUM_RESULTS_AT_A_TIME) do |i|" because infinity is the default
+0.step(Float::INFINITY, NUM_RESULTS_AT_A_TIME) do |f|
+  pull_requests = get_prs(f.to_i)
+
+  # TODO print the created at date, so a person can CtrlC whenever they want
+
+  if pull_requests["errors"]
+    puts "ERRORS: #{pull_requests.inspect}"
+    break
+  end
+  # binding.pry
+  pr = pr_containing_commit(pull_requests["values"], @commit)
+  break if ((pull_requests["size"] == 0) or pr)
+end
+
+if pr.nil?
+  puts "commit '#{@commit}' not found in any pr"
+else
+  binding.pry
+  puts "commit '#{@commit}' was found"
+end
+
 
 # ===================================
 # RESPONSE
 # ===================================
-puts "[#{Time.now}] Response: #{json_response.inspect}..."
-puts "Time: %0.1f secs" % [Time.now-start]
+#puts "Response: #{pull_requests}..."
 
