e5a2d068f0a237602e7bb7c78cc691a164f2812e
Gayle Bender <gayle.l.bender@jpmorgan.com>
2017-03-02 15:38:38 -0500

    also check south cafeteria

--- lunch_notifier.rb
+++ lunch_notifier.rb
@@ -6,22 +6,36 @@ require 'json'
 require 'pry'
 require 'csv'
 
-FOODS_I_WANT = ["PUMPKIN COCONUT BISQUE", "NACHO"]
-south_cafe = HTTParty.get("http://www.aramarkcafe.com/layouts/canary_2015/locationhome.aspx?locationid=4265&pageid=20&stationID=-1")
+FOODS_I_WANT = [/PUMPKIN COCONUT BISQUE/i, /NACHO/i, /WURST.*BIER/i]
 north_cafe = HTTParty.get("http://www.aramarkcafe.com/layouts/canary_2015/locationhome.aspx?locationid=4261&pageid=20&stationID=-1")
+south_cafe = HTTParty.get("http://www.aramarkcafe.com/layouts/canary_2015/locationhome.aspx?locationid=4265&pageid=20&stationID=-1")
 
 north_data = Nokogiri::HTML(north_cafe)
+south_data = Nokogiri::HTML(south_cafe)
+
 north_mon = north_data.css("#mondayColumn").text
 north_tue = north_data.css("#tuesdayColumn").text
 north_wed = north_data.css("#wednesdayColumn").text
 north_thu = north_data.css("#thursdayColumn").text
 north_fri = north_data.css("#fridayColumn").text
 
+south_mon = south_data.css("#mondayColumn").text
+south_tue = south_data.css("#tuesdayColumn").text
+south_wed = south_data.css("#wednesdayColumn").text
+south_thu = south_data.css("#thursdayColumn").text
+south_fri = south_data.css("#fridayColumn").text
+
 
 FOODS_I_WANT.each do |food|
-  puts "Mon, NORTH has #{food}" if north_mon.match /#{food}/i
-  puts "Tue, NORTH has #{food}" if north_tue.match /#{food}/i
-  puts "Wed, NORTH has #{food}" if north_wed.match /#{food}/i
-  puts "Thu, NORTH has #{food}" if north_thu.match /#{food}/i
-  puts "Fri, NORTH has #{food}" if north_fri.match /#{food}/i
+  puts "Mon, NORTH has #{food}" if north_mon.match food
+  puts "Tue, NORTH has #{food}" if north_tue.match food
+  puts "Wed, NORTH has #{food}" if north_wed.match food
+  puts "Thu, NORTH has #{food}" if north_thu.match food
+  puts "Fri, NORTH has #{food}" if north_fri.match food
+
+  puts "Mon, SOUTH has #{food}" if south_mon.match food
+  puts "Tue, SOUTH has #{food}" if south_tue.match food
+  puts "Wed, SOUTH has #{food}" if south_wed.match food
+  puts "Thu, SOUTH has #{food}" if south_thu.match food
+  puts "Fri, SOUTH has #{food}" if south_fri.match food
 end
