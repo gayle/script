ec1344be651e809ca9cbc8a414245e857f78b9f3
Gayle Bender <gayle.l.bender@jpmorgan.com>
2017-03-02 15:52:54 -0500

    dry it up

--- lunch_notifier.rb
+++ lunch_notifier.rb
@@ -3,6 +3,7 @@
 require 'httparty'
 require 'nokogiri'
 
+DAYS = ["monday", "tuesday", "wednesday", "thursday", "friday"]
 FOODS_I_WANT = [/PUMPKIN COCONUT BISQUE/i, /NACHO/i, /WURST.*BIER/i]
 north_cafe = HTTParty.get("http://www.aramarkcafe.com/layouts/canary_2015/locationhome.aspx?locationid=4261&pageid=20&stationID=-1")
 south_cafe = HTTParty.get("http://www.aramarkcafe.com/layouts/canary_2015/locationhome.aspx?locationid=4265&pageid=20&stationID=-1")
@@ -10,29 +11,12 @@ south_cafe = HTTParty.get("http://www.aramarkcafe.com/layouts/canary_2015/locati
 north_data = Nokogiri::HTML(north_cafe)
 south_data = Nokogiri::HTML(south_cafe)
 
-north_mon = north_data.css("#mondayColumn").text
-north_tue = north_data.css("#tuesdayColumn").text
-north_wed = north_data.css("#wednesdayColumn").text
-north_thu = north_data.css("#thursdayColumn").text
-north_fri = north_data.css("#fridayColumn").text
+DAYS.each do |day|
+  FOODS_I_WANT.each do |food|
+    north_food = north_data.css("##{day}Column").text
+    puts "#{day.capitalize}, NORTH has #{food}" if north_food.match food
 
-south_mon = south_data.css("#mondayColumn").text
-south_tue = south_data.css("#tuesdayColumn").text
-south_wed = south_data.css("#wednesdayColumn").text
-south_thu = south_data.css("#thursdayColumn").text
-south_fri = south_data.css("#fridayColumn").text
-
-
-FOODS_I_WANT.each do |food|
-  puts "Mon, NORTH has #{food}" if north_mon.match food
-  puts "Tue, NORTH has #{food}" if north_tue.match food
-  puts "Wed, NORTH has #{food}" if north_wed.match food
-  puts "Thu, NORTH has #{food}" if north_thu.match food
-  puts "Fri, NORTH has #{food}" if north_fri.match food
-
-  puts "Mon, SOUTH has #{food}" if south_mon.match food
-  puts "Tue, SOUTH has #{food}" if south_tue.match food
-  puts "Wed, SOUTH has #{food}" if south_wed.match food
-  puts "Thu, SOUTH has #{food}" if south_thu.match food
-  puts "Fri, SOUTH has #{food}" if south_fri.match food
+    south_food = south_data.css("##{day}Column").text
+    puts "#{day.capitalize}, SOUTH has #{food}" if south_food.match food
+  end
 end
