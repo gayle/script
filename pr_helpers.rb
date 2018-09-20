4b1b89cb6fcff756381887c0d578cda2da3e4b43
Gayle Craig <gayle.l.bender@jpmorgan.com>
2018-09-20 14:54:05 -0400

    new server

--- pr_helpers.rb
+++ pr_helpers.rb
@@ -36,7 +36,7 @@ module PrHelpers
   end
 
   def pr_url(repo, pr_id)
-    "https://bitbucketdc.jpmchase.net:8443/projects/RSAMBB/repos/#{repo}/pull-requests/#{pr_id}"
+    "https://bitbucketdc.jpmchase.net/projects/RSAMBB/repos/#{repo}/pull-requests/#{pr_id}"
   end
 
   def get_pr(id)
